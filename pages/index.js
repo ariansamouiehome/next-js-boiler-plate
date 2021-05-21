fa4d03058d65af9219d028abe84a25d230b623a8
734n <734n@C02FC2MWMD6T.home>
2021-05-21 09:30:18 +0100

    changed standard page information

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
-import Home from '../containers/Home';
+import PageComponents from '../containers/Home';
 const axios = require('axios');
 
-const Index = (props) => {
-    return (<Home data={props.data}/>)
+const Page = (props) => {
+    return (<PageComponents data={props.data}/>)
 }
 
-Index.getInitialProps = async () => {
+Page.getInitialProps = async () => {
     const res = await axios.get(`https://pokeapi.co/api/v2/pokemon`);
     const pageData = await res.data.results;
     return { data: pageData };
 };
 
-export default Index;
+export default Page;
